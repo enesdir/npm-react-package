c71a635c566cb1d87e3f8b561175cd33a85bb2a7
Enes Esen <enesen@gmx.com>
2024-01-05 01:48:52 +0300

    fix: eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,6 +37,8 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:react/jsx-runtime',
 		'prettier',
 		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:@typescript-eslint/recommended',
